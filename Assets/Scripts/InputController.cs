113dbe9af803b64eff560c206472d407a6261caa
zwarn <Michael.Budahn@gmail.com>
2015-07-11 13:18:57 +0200

    movement scripts

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	public PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float throttle = Mathf.Max(0,Input.GetAxis ("Horizontal"));
+		playerController.throttle (throttle);
+		float turn = Input.GetAxis ("Vertical");
+		playerController.turn (turn);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float acceleration;
+	public float turnSpeed;
+	private Rigidbody2D rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void throttle (float throttle)
+	{
+		rigidBody.AddForce (throttle * acceleration * transform.forward);
+	}
+
+	public void turn (float turn)
+	{
+		transform.RotateAround (transform.position, Vector3.forward, turn * turnSpeed);
+	}
+}
